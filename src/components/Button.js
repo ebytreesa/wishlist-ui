05684c8cfdf8d7e2d825d3b7024d4e838c51bd9a
eby <ebytreesa@gmail.com>
2021-06-03 08:08:37 +0200

    delete custom button component

--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Button = (props) => {
-    return (
-        <button className='btn' style={{backgroundColor:props.color}} onClick={props.onClick}>{props.text}</button>
-    )
-}
-export default Button
